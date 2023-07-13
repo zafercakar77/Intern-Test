92f5a41e0d88f8652aa58d9dece7445f989cc580
zafer <zafer@host.docker.internal>
2023-07-13 14:17:31 +0300

    add2

--- Scripts/deneme/Script1689246677327.groovy
+++ Scripts/deneme/Script1689246677327.groovy
@@ -17,3 +17,9 @@ import com.kms.katalon.core.windows.keyword.WindowsBuiltinKeywords as Windows
 import internal.GlobalVariable as GlobalVariable
 import org.openqa.selenium.Keys as Keys
 
+WebUI.openBrowser('google.com')
+
+WebUI.click(findTestObject(null))
+
+WebUI.closeBrowser()
+
